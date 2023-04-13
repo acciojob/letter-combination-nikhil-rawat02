2062bc14d8449473e2ecf84ae9bb6ab34ca6957d
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-13 15:18:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	function  helper(index, curr) {
+
+		if(index == input_digit.length){
+			if (curr.length > 1) {
+				res.push(curr);
+				return;
+			}
+		}
+
+		
+		let elem = input_digit[index];
+		for(let c for obj[elem]){
+			helper(index+1, curr + c);
+		}
+}
+	let obj ={
+		'0' = '0';
+		'1' = '1';
+		'2' = "abc";
+		'3'= "def"; 
+		'4'= "ghi";
+		'5' = "jkl";
+		'6' = "mno";
+		'7' = "pqrs";
+		'8' = "tuv";
+		'9'= "wxyz";
+	}
+
+	let res = [];
+	helper(0, "");
+
+	res.sort();
+	return res;
+
 }
 
 module.exports = letterCombinations;
