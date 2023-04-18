9b845f9bf769ae3b0f52a1a0eaeccd71421d2b47
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-18 17:46:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
-function  helper(index, curr) {
+var obj ={
+		'0' : '0',
+		'1' : '1',
+		'2' : "abc",
+		'3': "def",
+		'4': "ghi",
+		'5' : "jkl",
+		'6' : "mno",
+		'7' : "pqrs",
+		'8' : "tuv",
+		'9': "wxyz"
+	}
+var res;
+function  helper(index, curr, input_digit) {
 
 		if(index == input_digit.length){
-			if (curr.length > 1) {
+			if (curr.length >= 1) {
 				res.push(curr);
 				return;
 			}
 		}
 
-		let elem = input_digit[index];
-		for(let c for obj[elem]){
-			helper(index+1, curr + c);
-		}
+		let elem = obj[input_digit[index]];
+        for(let i =0; i < elem.length; i++){
+            helper(index+1,curr + elem.charAt(i),input_digit);
+        }
+		
 }
-
 function letterCombinations(input_digit) {
   //Complete the function
-	
-	let obj ={
-		'0' = '0';
-		'1' = '1';
-		'2' = "abc";
-		'3'= "def"; 
-		'4'= "ghi";
-		'5' = "jkl";
-		'6' = "mno";
-		'7' = "pqrs";
-		'8' = "tuv";
-		'9'= "wxyz";
-	}
-
-	let res = [];
-	helper(0, "");
+	res = [];
+    if(digits.length < 1)return res;
+	helper(0, "", digits);
 
 	res.sort();
 	return res;
 }
-
-module.exports = letterCombinations;
+console.log(letterCombinations(23));
